9cb68c301f355c99bad656fa915f6f1799f19a4e
jacksonf21 <jacksonf21@hotmail.com>
2019-09-18 21:12:22 -0400

    Updates

---
+++ precache-manifest.364b56b8d530253f1ac1c53c59796fcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0f1909cf6b3564ddaddba2ff4d2c6d",
+    "url": "/jacksonfung/index.html"
+  },
+  {
+    "revision": "f710dae2e8dad8efb0e1",
+    "url": "/jacksonfung/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "36212995a8b017648ed4",
+    "url": "/jacksonfung/static/js/2.cd930777.chunk.js"
+  },
+  {
+    "revision": "f710dae2e8dad8efb0e1",
+    "url": "/jacksonfung/static/js/main.462596f9.chunk.js"
+  },
+  {
+    "revision": "9ace7a68ded1a666dc05",
+    "url": "/jacksonfung/static/js/runtime~main.ff595ca9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jacksonfung/static/media/logo.5d5d9eef.svg"
+  }
+]);
