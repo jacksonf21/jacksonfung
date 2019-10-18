e4d700d231ee2cc884736a8d6dff09a999d583af
jacksonf21 <jacksonf21@hotmail.com>
2019-10-18 11:08:45 -0400

    added mapped skills to skill list

--- src/components/skill.component.jsx
+++ src/components/skill.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-// import '../styles/Main.css';
+import '../styles/Skill.css';
 
 export default function Skill({ name }) {
   return (  
     <div className={name}>
-      <img src={require(`../../public/icons/${name}.png`)} alt='' width='300px' height='300px'/>
+      <img src={require(`../../public/icons/${name}.png`)} alt='' width='125px' height='150px'/>
       {name}
     </div>
   )
